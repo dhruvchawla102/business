77e79e4a5f887ce4e2a45c1656f435a3f5bc533f
dhruvchawla102 <dhruvchawla102@gmail.com>
2020-10-02 21:10:01 +0530

    Updates

---
+++ precache-manifest.701c63607726f576c41e3ac7b06e28c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a8781e7d88116af0d408367e668299",
+    "url": "/business/index.html"
+  },
+  {
+    "revision": "7892dd46344fdd1d5959",
+    "url": "/business/static/css/main.e77ba179.chunk.css"
+  },
+  {
+    "revision": "16bc7cf5732465adce8e",
+    "url": "/business/static/js/2.69fcf4b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/business/static/js/2.69fcf4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7892dd46344fdd1d5959",
+    "url": "/business/static/js/main.bbe7e7f9.chunk.js"
+  },
+  {
+    "revision": "a68ab297a286d58feb16",
+    "url": "/business/static/js/runtime-main.d64ae901.js"
+  }
+]);
